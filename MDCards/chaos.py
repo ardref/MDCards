d4b888b8f164339b2e8d88ba3fc2633ef75c31f9
Richard Prosser <ardref@pm.me>
2022-05-05 22:46:01 +0100

    Basic card layout.

--- MDCards/chaos.py
+++ MDCards/chaos.py
@@ -1,23 +1,9 @@
 from kivy.lang import Builder
-
 from kivymd.app import MDApp
-
-KV = '''
-BoxLayout:
-    orientation: "vertical"
-
-    MDToolbar:
-        title: "MDToolbar"
-
-    MDLabel:
-        text: "Content"
-        halign: "center"
-'''
 
 
 class Chaos(MDApp):
     def build(self):
-#       return Builder.load_string(KV)
         return Builder.load_file('chaos.kv')
 
 
