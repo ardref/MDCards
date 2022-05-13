f18e8b4063c90190511c00d0e1f5403250242294
Richard Prosser <ardref@pm.me>
2022-05-13 19:44:40 +0100

    Config Parser added.

--- MDCards/chaos.py
+++ MDCards/chaos.py
@@ -1,10 +1,11 @@
 from kivymd.app import MDApp
+import configparser
 
 import csv
 import random
 
-CSV_FILE = 'chaos.csv'
-
+# N.B.: Kivy's own Config parser may be required in deployment, instead of the local .ini file.
+INI_FILE = 'chaos.ini'
 
 class ChaosApp(MDApp):
     """ Kivy app to manage 'Event Cards' """
@@ -12,14 +13,18 @@ class ChaosApp(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.deck = list()
-        self.readcsv()
+        config = configparser.ConfigParser()
+        config.read(INI_FILE)
+        self.csv_file = config.get('User', 'CSV_FILE')
 
-    def readcsv(self):
+        self.deck = list()
+
+    def read_csv(self):
         """ Populate Event deck from CSV file """
 
         self.deck.clear()
-        with open(CSV_FILE, newline='') as csvfile:
+
+        with open(self.csv_file, newline='') as csvfile:
             # Ignore first row - Header.
             rows = list(csv.reader(csvfile))[1:]
 
@@ -33,7 +38,7 @@ class ChaosApp(MDApp):
     def build(self):
         self.title = 'CHAnce Organising System'
 
-        config = self.config
+        self.read_csv()
 
         return
 
