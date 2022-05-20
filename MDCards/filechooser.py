1883a342f1572768a48085fc82bfe52659bd5389
Richard Prosser <ardref@pm.me>
2022-05-20 18:37:05 +0100

    Simplified Filechooser - Load only.

--- MDCards/filechooser.py
+++ MDCards/filechooser.py
@@ -14,15 +14,8 @@ class LoadDialog(FloatLayout):
     cancel = ObjectProperty(None)
 
 
-class SaveDialog(FloatLayout):
-    save = ObjectProperty(None)
-    text_input = ObjectProperty(None)
-    cancel = ObjectProperty(None)
-
-
 class Root(FloatLayout):
     loadfile = ObjectProperty(None)
-    savefile = ObjectProperty(None)
     text_input = ObjectProperty(None)
 
     def dismiss_popup(self):
@@ -31,13 +24,7 @@ class Root(FloatLayout):
     def show_load(self):
         content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
         self._popup = Popup(title="Load file", content=content,
-                            size_hint=(0.9, 0.9))
-        self._popup.open()
-
-    def show_save(self):
-        content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
-        self._popup = Popup(title="Save file", content=content,
-                            size_hint=(0.9, 0.9))
+                            size_hint=(0.6, 0.6))
         self._popup.open()
 
     def load(self, path, filename):
@@ -46,23 +33,16 @@ class Root(FloatLayout):
 
         self.dismiss_popup()
 
-    def save(self, path, filename):
-        with open(os.path.join(path, filename), 'w') as stream:
-            stream.write(self.text_input.text)
 
-        self.dismiss_popup()
-
-
-class Editor(App):
+class Filechooser(App):
     pass
 
 
 Factory.register('Root', cls=Root)
 Factory.register('LoadDialog', cls=LoadDialog)
-Factory.register('SaveDialog', cls=SaveDialog)
 
 
 if __name__ == '__main__':
-    Editor().run()
+    Filechooser().run()
 
 
