695c0b620f6da0c6c29be6ab0b5d37a1e9a045e2
Richard Prosser <ardref@pm.me>
2022-06-04 18:38:23 +0100

    CSV stuff. WIP.

--- MDCards/chaos.py
+++ MDCards/chaos.py
@@ -4,7 +4,7 @@ from kivy.uix.popup import Popup
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
 from kivy.logger import Logger, LOG_LEVELS
-
+from collections import namedtuple
 from os.path import exists
 
 import os
@@ -21,7 +21,11 @@ Logger.debug('ChaosApp: This is a debug message.')
 # TODO: Fill values in .kv 'card'
 
 # CSV file. Default (reset) is preceded by a '.'
-CSV_FILE = 'chaos.csv'
+CSV_FILE = '.chaos.csv'
+CSV_INIT = """1, CHAOS, Event Deck, Choose CSV File to Build Deck, Use 'Nav Bar' below: Back, Forward
+"""
+
+Fields = namedtuple('CSV_Header', 'Weight, Header, Title, Body, Extra')
 
 
 class LoadDialog(FloatLayout):
@@ -32,7 +36,6 @@ class LoadDialog(FloatLayout):
 
 class EventCard(FloatLayout):
     """ EventCard Root Rule in KV file """
-    loadfile = ObjectProperty(None)
     text_input = ObjectProperty(None)
 
     def __init__(self, **kwargs):
@@ -44,15 +47,17 @@ class EventCard(FloatLayout):
 
     def show_load(self):
         content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
-        self._popup = Popup(title="Load file", content=content,
-                            size_hint=(0.6, 0.6))
+        self._popup = Popup(title="Load file", content=content, size_hint=(0.6, 0.6))
         self._popup.open()
 
     def load(self, path, filename):
-        with open(os.path.join(path, filename[0])) as stream:
-            pass
-            self.text_input.text = stream.read()
-
+        # TODO: Update hidden file.
+        """ Validate CSV file, then save data to hidden file """
+        if len(filename) > 0:
+            with open(os.path.join(path, filename[0])) as stream:
+                rows = stream.read()
+                if not csv.Sniffer().has_header(rows):
+                    raise Exception("Header row not found in CSV file.")
         self.dismiss_popup()
 
     def selected(self, filename):
@@ -63,11 +68,13 @@ class ChaosApp(MDApp):
     """ Kivy app to manage 'Event Cards' """
 
     def __init__(self, **kwargs):
-        """ CSV file is created if it does not yet exist """
         super().__init__(**kwargs)
 
+        # CSV file is created if it does not yet exist.
         if not exists(CSV_FILE):
-            shutil.copyfile('.' + CSV_FILE, CSV_FILE)
+            with open(CSV_FILE, 'w') as f:
+                writer = csv.writer(f)
+                writer.writerows(CSV_INIT)
 
         self.deck = list()
 
@@ -76,12 +83,12 @@ class ChaosApp(MDApp):
 
         self.deck.clear()
 
-        with open(CSV_FILE, newline='') as csvfile:
+        with open(CSV_FILE) as csvfile:
             # Ignore first row - Header.
-            rows = list(csv.reader(csvfile))[1:]
+            reader = csv.DictReader(csvfile)
 
-            for row in rows:
-                self.deck.append(row)
+            for row in reader:
+                weight = int(row['Weight'])
 
     def shuffle(self):
         # N.B.: This method changes the original list, it does not return a new list.
